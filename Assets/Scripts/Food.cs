7185a2b0eb88ff297e8ba80565db0c82f8e299a1
RivadonS <rivadon.sung@bumail.net>
2026-02-13 15:50:55 +0700

    Add tag and add code

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
-    public int attackPoint = 5;
+    public int attackPoint = 10;
 
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Hit " + other.gameObject.name);
+        /*var health = other.gameObject.GetComponent<HealthV1>();
+        if (health != null)
+        {
+            health.TakeDamage(attackPoint);
+        }*/
+
+        if (other.gameObject.TryGetComponent<HealthV1>(out HealthV1 health))
+        {
+            health.TakeDamage(attackPoint);
+        }
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
+    public int hitCount = 0;
     void OnTriggerEnter(Collider other)
     {
-        
+        hitCount++;
+
+        if (hitCount >= 5)
+        {
+            Debug.Log("Game Over");
+            Time.timeScale = 0f;
+        }
     }
 }
